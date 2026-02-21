981ac1bd24b00d7ba2a54fb7eeaf164710e7ebc0
Udhayaboopathi <udhayaboopathi2003@gmail.com>
2026-02-21 22:53:03 +0530

    Initial CET project

---
+++ README.md
@@ -0,0 +1,5 @@
+This the Portal for periyar university PH.d entrance portal.
+
+kindly please update the exam link, departement in the data.json.
+
+once you update the data.json, just look the output in website
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PU_CET_July_Mock_2025</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css" />
+    <link rel="icon" href="/logo.png" />
+</head>
+<body>
+<?php include 'header.php'; ?>
+    <?php
+    // Read the JSON data from the file
+    $jsonData = file_get_contents('data.json');
+    // Convert JSON to PHP array
+    $data = json_decode($jsonData, true);
+    echo "<div class='container'>";
+    echo "<h1>Ph.D. Common Entrance Test (December 2025 Mock Session) : 01-08-2025</h1>";
+    echo "<div class='box-container'>";
+    // Function to display data in a box
+    function displayDataInBox($department,$current) {
+        echo '<div class="box">';
+        echo '<h1>' . $department['name'] . '</h1>';
+        echo '<div class="box-value">';
+        foreach ($department['value'] as $item) {
+            echo '<a href="'.$current.'.php?department=' . rawurlencode($item['val']) . '">' . $item['val'] . '</a>';
+        }
+        echo '</div>';
+        echo '</div>';
+    }
+    // Display Science Departments in separate boxes
+     displayDataInBox($data['arts'],"arts");
+    displayDataInBox($data['list1'],"list1");
+    displayDataInBox($data['list2'],"list2");
+   
+    echo "</div>";
+    echo "</div>";
+    ?>
+</body>
+</html>
